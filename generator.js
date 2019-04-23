120fa021e33ea53cd81d2af9b5cf714bf23e64b8
Brett <brett_matsumoto@yahoo.com>
2019-04-23 09:09:37 -1000

    should generate unique numbers in a random order

--- generator.js
+++ generator.js
@@ -3,7 +3,8 @@
 module.exports = function(n) {
   let unordered = [];
   for (let i = 0; i < n; i++) {
-    unordered.push(Math.floor(Math.random()*n))
+    unordered.push(Math.random() * n);
   }
-  return unordered
+  return unordered;
 };
+console.log(unordered);
