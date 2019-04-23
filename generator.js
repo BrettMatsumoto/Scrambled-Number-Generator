1ae55abe0ade8fae16e96d09e53e4666f3f5035a
Brett <brett_matsumoto@yahoo.com>
2019-04-23 09:05:24 -1000

    completed morning challenge

--- generator.js
+++ generator.js
@@ -1,3 +1,9 @@
+'use strict';
+
 module.exports = function(n) {
-  // do work here
+  let unordered = [];
+  for (let i = 0; i < n; i++) {
+    unordered.push(Math.floor(Math.random()*n))
+  }
+  return unordered
 };
